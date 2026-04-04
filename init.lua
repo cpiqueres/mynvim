912ae8a5d7ff61781e10f644b7d7a36f3dbf2f12
cpiqueres <carlos.piqueres@gmail.com>
2026-04-04 20:08:51 +0200

    added autocomplete info

--- init.lua
+++ init.lua
@@ -6,3 +6,29 @@ require('basics.keymaps')
 
 vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('pyright')
+
+-- Enable the new 0.12 completion features
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Trigger completion automatically as you type (Native hack)
+vim.api.nvim_create_autocmd("InsertCharPre", {
+  callback = function()
+    if vim.fn.pumvisible() == 0 then
+      local char = vim.v.char
+      -- Only trigger if the character is a letter or a trigger (like . or :)
+      if char:match("[%w%.%:]") then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-x><C-o>", true, false, true), "n", true)
+      end
+    end
+  end,
+})
+
+
+-- add the messages for E and W
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false })
+  end,
+})
+
+vim.opt.updatetime = 300
--- lua/basics/keymaps.lua
+++ lua/basics/keymaps.lua
@@ -9,5 +9,8 @@ vim.g.maplocalleader = "\\"
 -- in terminal model, Ctrl-t goes to normal mode...
 vim.keymap.set('t','<C-t>',[[<C-\><C-n>]],{noremap = true})
 
+-- panes keymaps
 vim.keymap.set('n','<C-Left>','<C-w>h<CR>',{noremap = true})
 vim.keymap.set('n','<C-Right>','<C-w>l<CR>',{noremap = true})
+vim.keymap.set('n','<C-Up>','<C-w>k<CR>',{noremap = true})
+vim.keymap.set('n','<C-Down>','<C-w>j<CR>',{noremap = true})
